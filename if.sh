689c6f5b987db977de44ceaa7c022fe6e3f3420f
Ankita-Modak <127712346+Ankita-Modak@users.noreply.github.com>
2023-03-18 08:55:36 +0530

    Add files via upload

---
+++ if.sh
@@ -0,0 +1,8 @@
+read -p "Enter the num: " a
+read -p "Enter the num: " b
+if($a -gt $b)
+then
+echo $a is greater than $b
+else 
+echo $b is greater than $a
+
---
+++ sum.sh
@@ -0,0 +1,6 @@
+read -p "Enter the first num: " x
+read -p "Enter the second num: " y
+echo $x
+echo $y
+z=$(($x + $y))
+echo "Sum is $z"
